6b6a7a8217aed63ecacf3c8493128d7d62a3f181
Shari Meggs <smeggs1@gmail.com>
2016-11-16 15:40:36 -0800

    Have decided to restructure and push through on this hash like scheme

--- scrabble.js
+++ scrabble.js
@@ -1,5 +1,5 @@
-var Scrabble = function() {
-  this.scoreChart = {
+Scrabble =  {
+  scoreChart: {
     1: ["A","E", "I", "O", "U", "L", "N", "R", "S", "T"],
     2: ["D", "G"],
     3: ["B", "C", "M", "P"],
@@ -7,30 +7,34 @@ var Scrabble = function() {
     5: ["K"],
     8: ["J", "X"],
     10: ["Q", "Z"]
-  };
+  },
+
+  // Object.prototype.getKeyByValue = function( value ) {
+  //   for( var prop in Scrabble.scoreChart ) {
+  //     if( Scrabble.scoreChart.hasOwnProperty( prop ) ) {
+  //       if( Scrabble.scoreChart[ prop ] === value )
+  //       return prop;
+  //     }
+  //   }
+  // },
+  score: function(word) {
+    var score = 0;
+    var upWord = word.toUpperCase();
+    for (i = 0; i < upWord.length; i++) {
+
+
+      console.log(">>>>>>>>" + Scrabble.scoreChart.getKeyByValue(upWord[i]));
+      score += parseInt(Scrabble.scoreChart.getKeyByValue(upWord[i]));
+      console.log("+++++++++++++++++++" + score);
+
+    }
+    return score;
+  },
 };
 
-Object.prototype.getKeyByValue = function( value ) {
-  for( var prop in this.scoreChart ) {
-    if( this.scoreChart.hasOwnProperty( prop ) ) {
-      if( this.scoreChart[ prop ] === value )
-      return prop;
-    }
-  }
-};
 
-Scrabble.prototype.score = function(word) {
-var score = 0;
-  var upWord = word.toUpperCase();
-  for (i = 0; i < upWord.length; i++) {
-    if (Object.values(this.scoreChart).toString().includes(upWord[i])) {
-      console.log(">>>>>>>>" + (this.scoreChart).getKeyByValue(upWord[i]));
-     score += parseInt(this.scoreChart.getKeyByValue(upWord[i]));
-     console.log("+++++++++++++++++++" + score);
-    }
-  }
-  return score;
-};
+
+
 
 // YOUR CODE HERE
 Scrabble.prototype.helloWorld = function() {
